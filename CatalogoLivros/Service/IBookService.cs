3291bb33f7c29e4fe89efd74d33813012df7f12e
malves85 <marceloa153@gmail.com>
2022-12-13 10:22:46 +0000

    Criação do serviço e controlador da api

---
+++ CatalogoLivros/Service/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoLivros.Models;
+
+namespace CatalogoLivros.Service
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetBooks();
+        Task<Book> GetBookById(int id);
+        Task<IEnumerable<Book>> GetBookByTitle(string title);
+        Task CreateBook(Book book);
+        Task UpdateBook(Book book);
+        Task DeleteBookById(Book book);
+
+    }
+}
+
