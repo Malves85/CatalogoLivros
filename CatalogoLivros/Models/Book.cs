767b542ee5046cd29d18677d7904ccb837461de4
malves85 <marceloa153@gmail.com>
2022-12-20 09:49:11 +0000

    inserido soft delete

--- CatalogoLivros/Models/Book.cs
+++ CatalogoLivros/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CatalogoLivros.Models
 {
@@ -15,7 +16,10 @@ namespace CatalogoLivros.Models
         [StringLength(80)]
         public string Author { get; set; }
         [Required]
+        [Range( 0, 999, ErrorMessage = "Price must be >= 0")]
         public double Price { get; set; }
+        [DefaultValue(false)]
+        public bool isDeleted { get; set; }
 
     }
 }
